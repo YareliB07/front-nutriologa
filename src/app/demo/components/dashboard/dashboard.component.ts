ea2afa2cbaea52b846c802c34411d7fb8b68b446
YareliB07 <yarelibravo84@gmail.com>
2023-11-08 21:30:45 -0600

    update code

--- src/app/demo/components/dashboard/dashboard.component.ts
+++ src/app/demo/components/dashboard/dashboard.component.ts
@@ -49,6 +49,16 @@ export class DashboardComponent implements OnInit {
 
     activeTabIndex: number =0;
 
+    editProduct(product: Product) {
+        this.product = { ...product };
+        this.productDialog = true;
+    }
+
+    deleteProduct(product: Product) {
+        this.deleteProductDialog = true;
+        this.product = { ...product };
+    }
+
     confirmDelete() {
         this.deleteProductDialog = false;
         this.products = this.products.filter(val => val.id !== this.product.id);
@@ -56,10 +66,56 @@ export class DashboardComponent implements OnInit {
         this.product = {};
     }
 
-    editPaciente(product: Product) {
-        this.product = { ...product };
-        this.productDialog = true;
+    hideDialog() {
+        this.productDialog = false;
+        this.submitted = false;
     }
+
+    saveProduct() {
+        this.submitted = true;
+
+        if (this.product.name?.trim()) {
+            if (this.product.id) {
+                // @ts-ignore
+                this.product.inventoryStatus = this.product.inventoryStatus.value ? this.product.inventoryStatus.value : this.product.inventoryStatus;
+                this.products[this.findIndexById(this.product.id)] = this.product;
+                this.messageService.add({ severity: 'success', summary: 'Successful', detail: 'Product Updated', life: 3000 });
+            } else {
+                this.product.id = this.createId();
+                this.product.code = this.createId();
+                // @ts-ignore
+                this.product.inventoryStatus = this.product.inventoryStatus ? this.product.inventoryStatus.value : 'INSTOCK';
+                this.products.push(this.product);
+                this.messageService.add({ severity: 'success', summary: 'Successful', detail: 'Product Created', life: 3000 });
+            }
+
+            this.products = [...this.products];
+            this.productDialog = false;
+            this.product = {};
+        }
+    }
+
+    findIndexById(id: string): number {
+        let index = -1;
+        for (let i = 0; i < this.products.length; i++) {
+            if (this.products[i].id === id) {
+                index = i;
+                break;
+            }
+        }
+
+        return index;
+    }
+
+    createId(): string {
+        let id = '';
+        const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        for (let i = 0; i < 5; i++) {
+            id += chars.charAt(Math.floor(Math.random() * chars.length));
+        }
+        return id;
+    }
+    
 
     openExp() {
         this.submitted = false;
@@ -85,6 +141,18 @@ export class DashboardComponent implements OnInit {
        
     }
 
+    openNew() {
+        this.product = {};
+        this.submitted = false;
+        this.productDialog = true;
+    }
+
+
+    onFilter(dv: DataView, event: Event) {
+        dv.filter((event.target as HTMLInputElement).value);
+    }
+    
+    
     selectedInfecciosas: string[] = [];
     selectedCronicas: string[] = [];
     selectedConsumo: string[] = [];
@@ -104,46 +172,6 @@ export class DashboardComponent implements OnInit {
     controlPeso!: string;
     obtuvoResultado!: string;
     medicamentosBajar!: string;
-
-
-    openNew() {
-        this.product = {};
-        this.submitted = false;
-        this.productDialog = true;
-    }
-
-    hideDialog() {
-        this.productDialog = false;
-        this.submitted = false;
-    }
-
-    onFilter(dv: DataView, event: Event) {
-        dv.filter((event.target as HTMLInputElement).value);
-    }
-    
-    saveProduct() {
-        this.submitted = true;
-
-        if (this.product.name?.trim()) {
-            if (this.product.id) {
-                // @ts-ignore
-                this.product.inventoryStatus = this.product.inventoryStatus.value ? this.product.inventoryStatus.value : this.product.inventoryStatus;
-                // this.products[this.findIndexById(this.product.id)] = this.product;
-                this.messageService.add({ severity: 'success', summary: 'Successful', detail: 'Product Updated', life: 3000 });
-            } else {
-                // this.product.id = this.createId();
-                // this.product.code = this.createId();
-                this.product.image = 'product-placeholder.svg';
-                // @ts-ignore
-                this.product.inventoryStatus = this.product.inventoryStatus ? this.product.inventoryStatus.value : 'INSTOCK';
-                this.products.push(this.product);
-                this.messageService.add({ severity: 'success', summary: 'Successful', detail: 'Product Created', life: 3000 });
-            }
-
-            this.products = [...this.products];
-            this.productDialog = false;
-            this.product = {};
-        }
-    }
+    cirugiaPeso!: string;
 
 }
